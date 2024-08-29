1c775daac62fa7539e416309c5b4490219703943
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2024-08-29 20:25:42 +0530

    added progress bar

--- scraper/times_scraper.py
+++ scraper/times_scraper.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup as bs
+from tqdm import tqdm
 
 def get_json(topic, count = 30):
     url = "https://toifeeds.indiatimes.com/treact/feeds/toi/web/show/topic"
@@ -22,13 +23,10 @@ def get_content(url):
     response = requests.get(url)
     if response.status_code != 200:
         return None
-    
     soup = bs(response.content, 'html.parser')
     content = soup.find('div', attrs={'data-articlebody': "1"})
-
     time = soup.find('div', attrs={'class': "pZFl7"})
     time  = time.find_all('span')
-
     return [content.text if content else None, time[1].text if time else None]
 
 def get_articles(topic, count = 30):
@@ -36,14 +34,13 @@ def get_articles(topic, count = 30):
     if data is None:
         return None
 
+    pbar = tqdm(total = count)
+
     articles = []
     for article in data['contentsData']['items']:
-
         title = article['hl']
         title = re.sub(r'<\/?i.*?>', '', title)
-
         content_time = get_content(article['wu'])
-        
         articles.append({
             'title': title,
             'url': article['wu'],
@@ -51,7 +48,8 @@ def get_articles(topic, count = 30):
             'content': content_time[0],
             'time': content_time[1]
         })
+        pbar.update(1)
 
     return articles
 
-# print(get_articles('cloudburst', 5))
+# print(len(get_articles('cloudburst')))
